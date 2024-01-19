9317b9f016a9800df03ff97eb4f52e25d04f7e0a
Yaroslav Tsalko <yariktb124@gmail.com>
2024-01-19 19:21:40 +0200

    Initialization and init feature

---
+++ ga.sh
@@ -0,0 +1,238 @@
+#!/bin/bash
+
+#for comfort
+#rm -rf .git
+TMPFILE=$(mktemp)
+NC='\033[0m'
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+DONE="${GREEN}
+
+================================
+ ______   ____ __  _________
+ |  __ \ / __ \| \ | |  ____|
+ | |  | | |  | |  \| | |__
+ | |  | | |  | | .   |  __|
+ | |__| | |__| | |\  | |____
+ |_____/ \____/|_| \_|______|
+
+================================
+${NC}
+"
+
+
+echo -e "${GREEN}WELCOME TO:'
+  ______ _____ _______      _______ _______ _______ _____ _______ _______ _______ __   _ _______
+ |  ____   |      |         |_____| |______ |______   |   |______    |    |_____| | \  |    |
+ |_____| __|__    |         |     | ______| ______| __|__ ______|    |    |     | |  \_|    |
+
+"
+echo -e "${NC}"
+read -p  "Press any key to continue> "
+
+
+function multiselect {
+    # little helpers for terminal print control and key input
+    ESC=$( printf "\033")
+    cursor_blink_on()   { printf "$ESC[?25h"; }
+    cursor_blink_off()  { printf "$ESC[?25l"; }
+    cursor_to()         { printf "$ESC[$1;${2:-1}H"; }
+    print_inactive()    { printf "$2   $1 "; }
+    print_active()      { printf "$2  $ESC[7m $1 $ESC[27m"; }
+    get_cursor_row()    { IFS=';' read -sdR -p $'\E[6n' ROW COL; echo ${ROW#*[}; }
+
+    local return_value=$1
+    local -n options=$2
+    local -n defaults=$3
+
+    local selected=()
+    for ((i=0; i<${#options[@]}; i++)); do
+        if [[ ${defaults[i]} = "true" ]]; then
+            selected+=("true")
+        else
+            selected+=("false")
+        fi
+        printf "\n"
+    done
+
+    # determine current screen position for overwriting the options
+    local lastrow=`get_cursor_row`
+    local startrow=$(($lastrow - ${#options[@]}))
+
+    # ensure cursor and input echoing back on upon a ctrl+c during read -s
+    trap "cursor_blink_on; stty echo; printf '\n'; exit" 2
+    cursor_blink_off
+
+    key_input() {
+        local key
+        IFS= read -rsn1 key 2>/dev/null >&2
+        if [[ $key = ""      ]]; then echo enter; fi;
+        if [[ $key = $'\x20' ]]; then echo space; fi;
+        if [[ $key = "k" ]]; then echo up; fi;
+        if [[ $key = "j" ]]; then echo down; fi;
+        if [[ $key = $'\x1b' ]]; then
+            read -rsn2 key
+            if [[ $key = [A || $key = k ]]; then echo up;    fi;
+            if [[ $key = [B || $key = j ]]; then echo down;  fi;
+        fi
+    }
+
+    toggle_option() {
+        local option=$1
+        if [[ ${selected[option]} == true ]]; then
+            selected[option]=false
+        else
+            selected[option]=true
+        fi
+    }
+
+    print_options() {
+        # print options by overwriting the last lines
+        local idx=0
+        for option in "${options[@]}"; do
+
+
+            cursor_to $(($startrow + $idx))
+            if [ $idx -eq $1 ]; then
+                print_active "$option" "$prefix"
+            else
+                print_inactive "$option" "$prefix"
+            fi
+            ((idx++))
+        done
+    }
+
+    local active=0
+    while true; do
+        print_options $active
+
+        # user key control
+        case `key_input` in
+            space)  toggle_option $active;;
+            enter)  print_options -1; break;;
+            up)     ((active--));
+                    if [ $active -lt 0 ]; then active=$((${#options[@]} - 1)); fi;;
+            down)   ((active++));
+                    if [ $active -ge ${#options[@]} ]; then active=0; fi;;
+        esac
+    done
+
+    # cursor position back to normal
+    cursor_to $lastrow
+    printf "\n"
+    cursor_blink_on
+
+    eval $return_value='("${selected[@]}")'
+}
+
+
+function start() {
+    clear
+    echo "Current dir: $(pwd)"
+    echo
+
+    if git status &> /dev/null; then
+        GITSTATUS=0
+        echo -e "${GREEN}Git status successful${NC}"
+        read -p "Press any key to move forward> "
+        git_operations
+    else
+
+        echo -e "${RED}Git status failed${NC}"
+        echo "There're no existing git repo in this dir, what your actions will be?"
+        echo
+        echo
+        echo
+        echo
+        gitinit
+
+    fi
+
+    echo "STATUS is $GITSTATUS"
+}
+
+function gitinit() {
+    while true;
+    do
+
+        echo "Type <help> for the comand list!"
+        read -p "> " command
+        case $command in
+            help)
+
+                echo "
+                <init>        - create git repo
+                <clone>       - clone existing repo from GitHub/Lab>
+                <exit>
+                "
+                ;;
+            init)
+
+                git init
+                while true;
+                do
+                    echo "Write name for remote repo, and paste link, example: <origin git@github.com:...> "
+                    read -p "> " remoname remolink
+                    if [ -z "$remoname" ]; then
+                        echo -e "${RED} ERROR! There is no remote repo name!${NC}"
+                        continue
+                    elif [ -z "$remolink" ]; then
+                        echo -e "${RED} ERROR! There is no remote repo url!${NC}"
+                        continue
+                    else
+                        if git remote add $remoname $remolink &> /dev/null; then
+                            git pull
+                            echo "Pulled origin"
+                            echo "${DONE}"
+                            read -p "Press any key to move forward> "
+                            git_operations
+                        else
+                          echo -e "${RED}Something went wrong!${NC}"
+                          break
+                        fi
+
+                        break
+                    fi
+                done
+
+                ;;
+            clone)
+
+                read -p "Paste link of your remote repo: " remorepolink
+                if git clone $remorepolink &> /dev/null; then
+                     echo "${DONE}"
+                     read -p "Press any key to move forward> "
+                     git_operations
+                else
+                    echo -e "${RED}Remote repo link is empty!${NC}"
+                    continue
+                fi
+
+                ;;
+            exit)
+                clear
+                break;
+                ;;
+            *)
+                echo -e "${RED}ERROR:${NC} No such command!"
+                ;;
+
+        esac
+    done
+}
+
+function git_operations() {
+    clear
+    echo "Choose operation: "
+
+    my_options=(   "add"  "commit"  "push" )
+    preselection=( "true"      "true"      "false"    )
+
+    multiselect result my_options preselection
+    #kill $$
+}
+
+
+
+
+start
